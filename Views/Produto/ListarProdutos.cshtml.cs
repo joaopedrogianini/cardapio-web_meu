738e902df8989e858489b0abe57dcae10fe3907d
joaopedrogianini <joaopedrogianinifranzin@gmail.com>
2024-08-01 10:48:09 -0300

    salva

---
+++ Views/Produto/ListarProdutos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CardapioWeb.Views
+{
+    public class produtoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
